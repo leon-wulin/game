84bf9438660440fc303c815068b64c9558e75bb8
leon-wulin <55013445+leon-wulin@users.noreply.github.com>
2019-12-09 09:06:34 +0800

    updated

---
+++ game/main.py
@@ -0,0 +1,35 @@
+# -*- coding: UTF-8 -*-
+# writen by leon
+# writen time: 2019/12/7
+
+
+
+import tkinter as tk
+from tkinter import ttk
+import tkinter.messagebox
+from PIL import Image, ImageTk
+
+
+print('---------------------------------------------------------------------------------------------------------------')
+print('|                                                                                                              |')
+print('|                                                                                                              |')
+print('|                                                                                                              |')
+print('|                                                                                                              |')
+print('|                                                                                                              |')
+print('|                                                                                                              |')
+print('|                                                                                                              |')
+print('|                                                                                                              |')
+print('|                                                                                                              |')
+print('|                                                                                                              |')
+print('|                             start game:print 1                                                               |')
+print('|                                                                                                              |')
+print('|                                                                                                              |')
+print('|                                                                                                              |')
+print('|                                                                                                              |')
+print('|                                                                                                              |')
+print('|                                                                                                              |')
+print('|                                                                                                              |')
+print('|                                                                                                              |')
+print('|                                                                                                              |')
+print('|                                                                                                              |')
+print('________________________________________________________________________________________________________________')
---
+++ math/math_caculation.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+# writen by leon
+# writen time: 2019/12/7
+
+
+
+x=4
+a=x**3
+print(a)
